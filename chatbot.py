e4a9126dbd642833e63074416d43f7942dbfdee4
DKT2234 <khoita2210@gmail.com>
2025-12-26 10:10:41 +0700

    Fetch the model and initialize a tokeniser

--- chatbot.py
+++ chatbot.py
@@ -2,3 +2,6 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 model_name = "facebook/blenderbot-400M-distill"
 
+#Fetch the model and initialize a tokeniser
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
