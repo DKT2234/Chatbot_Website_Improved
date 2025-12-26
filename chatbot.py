166f9058827b9f7f71ccbfcc85df5dc1190915bd
DKT2234 <khoita2210@gmail.com>
2025-12-26 10:14:00 +0700

    Add code to keeping track of conversation history and encoding the conversation history

--- chatbot.py
+++ chatbot.py
@@ -5,3 +5,12 @@ model_name = "facebook/blenderbot-400M-distill"
 #Fetch the model and initialize a tokeniser
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+#Keeping track of conversation history
+
+conversation_history = []
+
+#Encoding the conversation history
+
+history_string = "\n".join(conversation_history)
+
